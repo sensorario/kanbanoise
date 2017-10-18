293a32c5f8882ae013d96a44a42ad5821315cefb
Simone Gentili <sensorario@gmail.com>
2017-10-18 03:53:17 +0200

    close #10 - apply colors to card

--- rebuild.sh
+++ rebuild.sh
@@ -4,6 +4,8 @@ php ./bin/console doctrine:database:create --env=test
 php ./bin/console doctrine:schema:update --force --env=test
 
 php ./bin/console doctrine:query:sql --env=test "insert into member values (null, 'sensorario');"
+php ./bin/console doctrine:query:sql --env=test "insert into member values (null, 'deadpool');"
+php ./bin/console doctrine:query:sql --env=test "insert into member values (null, 'doctor strange');"
 
 php ./bin/console doctrine:query:sql --env=test "insert into card_type values (null, 'task');"
 php ./bin/console doctrine:query:sql --env=test "insert into card_type values (null, 'bug');"
@@ -14,3 +16,5 @@ php ./bin/console doctrine:query:sql --env=test "insert into status values (null
 php ./bin/console doctrine:query:sql --env=test "insert into status values (null, 'done', 4, null);"
 
 php ./bin/console doctrine:query:sql --env=test "insert into card values (null, 'release version 1.0.0', 'add lot of features and tag version 1.0.0', 'in progress', 'sensorario', 'task');"
+php ./bin/console doctrine:query:sql --env=test "insert into card values (null, 'save magic', 'save the magic from Empirikul', 'todo', 'doctor strange', 'task');"
+php ./bin/console doctrine:query:sql --env=test "insert into card values (null, 'useless hero', 'exists this useless hero', 'done', 'doctor strange', 'bug');"
