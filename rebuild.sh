e27ca15f50bf26851c4b314870aa694defdea63c
Simone Gentili <sensorario@gmail.com>
2017-10-18 04:32:37 +0200

    add default statuses and card types

--- rebuild.sh
+++ rebuild.sh
@@ -2,3 +2,11 @@
 php ./bin/console doctrine:database:drop --force --env=test
 php ./bin/console doctrine:database:create --env=test
 php ./bin/console doctrine:schema:update --force --env=test
+
+php ./bin/console doctrine:query:sql --env=test "insert into status values (null, 'backlog', 1, null);"
+php ./bin/console doctrine:query:sql --env=test "insert into status values (null, 'todo', 2, null);"
+php ./bin/console doctrine:query:sql --env=test "insert into status values (null, 'in progress', 3, 1);"
+php ./bin/console doctrine:query:sql --env=test "insert into status values (null, 'done', 4, null);"
+
+php ./bin/console doctrine:query:sql --env=test "insert into card_type values (null, 'task');"
+php ./bin/console doctrine:query:sql --env=test "insert into card_type values (null, 'bug');"
