3186c5a387829d836e0eece43456767549bdbcba
Simone Gentili <sensorario@gmail.com>
2017-11-06 05:33:05 +0100

    add doctrine fixtures to install project

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
  - git clone https://github.com/sensorario/kanbanoise
  - cd kanbanoise
  - composer install
- - ./rebuild.sh
+ - ./bin/console doctrine:fixtures:load --no-interaction
  - ./bin/console server:start
  - open http://localhost:8000 in your browser
  
--- src/AppBundle/DataFixtures/ORM/Users.php
+++ src/AppBundle/DataFixtures/ORM/Users.php
@@ -5,14 +5,50 @@ namespace AppBundle\DataFixtures\ORM;
 use AppBundle\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Kanban\Factories\UserFactory;
+use Kanban\Factories;
 
 class Users extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $admin = UserFactory::buildAdmin();
+        $admin = Factories\UserFactory::buildAdmin();
         $manager->persist($admin);
+        $manager->flush();
+
+        $board = Factories\BoardFactory::buildWithOwner('admin');
+        $manager->persist($board);
+        $manager->flush();
+
+        $defaultStatuses = [[
+            'name' => 'Backlog',
+            'wip'  => 0,
+        ], [
+            'name' => 'To Do',
+            'wip'  => 0,
+        ], [
+            'name' => 'In Progress',
+            'wip'  => 3,
+        ], [
+            'name' => 'Done',
+            'wip'  => 0,
+        ]];
+
+        foreach ($defaultStatuses as $itemValue) {
+            $status = Factories\StatusFactory::buildWithNameAndWipLimit(
+                $itemValue['name'],
+                $itemValue['wip']
+            );
+
+            $manager->persist($status);
+            $manager->flush();
+        }
+
+        $status = Factories\CardTypeFactory::buildWithName('task');
+        $manager->persist($status);
+        $manager->flush();
+
+        $project = Factories\ProjectFactory::buildWithNameAndOwner('kanbanoise', 'admin');
+        $manager->persist($project);
         $manager->flush();
     }
 }
